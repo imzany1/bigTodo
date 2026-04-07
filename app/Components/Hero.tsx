22dd129f962b7e6c786247d551971551aea8441c
Hussain Abbas <sayedhusainabbashaider78600@gmail.com>
2026-04-07 11:58:40 +0530

    Edit state IN

--- app/Components/Hero.tsx
+++ app/Components/Hero.tsx
@@ -6,13 +6,14 @@ import Right from "./Right";
 type HeroProps = {
   taskList: Task[];
   setTaskList: React.Dispatch<React.SetStateAction<Task[]>>
+  updateProgress: Function
 };
 
-export default function Hero({ taskList, setTaskList }: HeroProps){
+export default function Hero({ taskList, setTaskList,updateProgress }: HeroProps){
     const [selectedTaskid, setSelectedTaskid] = useState("")
     return(
-        <div className="bg-zinc-800 absolute top-[57%] left-[50%] translate-[-50%] h-120 w-200 rounded-md p-4 flex gap-4">
-            <Left taskList={taskList} selectedTaskid = {selectedTaskid} setSelectedTaskid={setSelectedTaskid} />
+        <div className="bg-zinc-800 absolute top-[57%] left-[50%] translate-[-50%] h-120 w-240 rounded-md p-4 flex gap-4">
+            <Left taskList={taskList} selectedTaskid = {selectedTaskid} setSelectedTaskid={setSelectedTaskid} updateProgress={updateProgress} />
             <Right setTaskList={setTaskList} taskList={taskList} selectedTaskid = {selectedTaskid} />
         </div>
     )
--- app/Components/Left.tsx
+++ app/Components/Left.tsx
@@ -4,12 +4,13 @@ type LeftProps = {
     taskList : Task[],
     setSelectedTaskid : any,
     selectedTaskid: any
+    updateProgress: Function
 }
 
-export default function Left({taskList, setSelectedTaskid, selectedTaskid}: LeftProps){
+export default function Left({taskList, setSelectedTaskid, selectedTaskid, updateProgress}: LeftProps){
     return(
         <div className=" border w-[30%] rounded-lg overflow-y-auto p-3 flex flex-col gap-2 [scrollbar-width:none] ">
-            {taskList.map((task)=>(<Card task={(task)} setSelectedTaskid={setSelectedTaskid} selectedTaskid={selectedTaskid} key={task.id}/>))}
+            {taskList.map((task)=>(<Card task={(task)} setSelectedTaskid={setSelectedTaskid} selectedTaskid={selectedTaskid} key={task.id} updateProgress={updateProgress} />))}
         </div>
     )
 }
